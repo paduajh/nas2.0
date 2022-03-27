8923f3d1ac767d398fd5b964d23bea6b95193d70
Pedro Henrique de Campos Silva <pedrohsilva04@gmail.com>
2022-03-27 13:37:46 -0300

    base project

--- config/infyom/laravel_generator.php
+++ config/infyom/laravel_generator.php
@@ -13,43 +13,43 @@ return [
 
         'migration'         => database_path('migrations/'),
 
-        'model'             => app_path('Models/'),
+        'model'             => app_path('Models/PreCadastro'),
 
-        'datatables'        => app_path('DataTables/'),
+        'datatables'        => app_path('DataTables/PreCadastro'),
 
-        'repository'        => app_path('Repositories/'),
+        'repository'        => app_path('Repositories/PreCadastro'),
 
         'routes'            => base_path('routes/web.php'),
 
         'api_routes'        => base_path('routes/api.php'),
 
-        'request'           => app_path('Http/Requests/'),
+        'request'           => app_path('Http/Requests/PreCadastro'),
 
-        'api_request'       => app_path('Http/Requests/API/'),
+        'api_request'       => app_path('Http/Requests/API/PreCadastro'),
 
-        'controller'        => app_path('Http/Controllers/'),
+        'controller'        => app_path('Http/Controllers/PreCadastro'),
 
-        'api_controller'    => app_path('Http/Controllers/API/'),
+        'api_controller'    => app_path('Http/Controllers/API/PreCadastro'),
 
-        'api_resource'      => app_path('Http/Resources/'),
+        'api_resource'      => app_path('Http/Resources/PreCadastro'),
 
-        'repository_test'   => base_path('tests/Repositories/'),
+        'repository_test'   => base_path('tests/Repositories/PreCadastro'),
 
-        'api_test'          => base_path('tests/APIs/'),
+        'api_test'          => base_path('tests/APIs/PreCadastro'),
 
-        'tests'             => base_path('tests/'),
+        'tests'             => base_path('tests/PreCadastro'),
 
-        'views'             => resource_path('views/'),
+        'views'             => resource_path('views/pre_cadastro'),
 
-        'schema_files'      => resource_path('model_schemas/'),
+        'schema_files'      => resource_path('model_schemas/pre_cadastro'),
 
         'templates_dir'     => resource_path('infyom/infyom-generator-templates/'),
 
-        'seeder'            => database_path('seeders/'),
+        'seeder'            => database_path('seeders/pre_cadastro'),
 
         'database_seeder'   => database_path('seeders/DatabaseSeeder.php'),
 
-        'factory'           => database_path('factories/'),
+        'factory'           => database_path('factories/pre_cadastro'),
 
         'view_provider'     => app_path('Providers/ViewServiceProvider.php'),
     ],
@@ -63,31 +63,31 @@ return [
 
     'namespace' => [
 
-        'model'             => 'App\Models',
+        'model'             => 'App\Models\PreCadastro',
 
-        'datatables'        => 'App\DataTables',
+        'datatables'        => 'App\DataTables\PreCadastro',
 
-        'repository'        => 'App\Repositories',
+        'repository'        => 'App\Repositories\PreCadastro',
 
-        'controller'        => 'App\Http\Controllers',
+        'controller'        => 'App\Http\Controllers\PreCadastro',
 
-        'api_controller'    => 'App\Http\Controllers\API',
+        'api_controller'    => 'App\Http\Controllers\API\PreCadastro',
 
-        'api_resource'      => 'App\Http\Resources',
+        'api_resource'      => 'App\Http\Resources\PreCadastro',
 
-        'request'           => 'App\Http\Requests',
+        'request'           => 'App\Http\Requests\PreCadastro',
 
-        'api_request'       => 'App\Http\Requests\API',
+        'api_request'       => 'App\Http\Requests\API\PreCadastro',
 
-        'seeder'            => 'Database\Seeders',
+        'seeder'            => 'Database\Seeders\PreCadastro',
 
-        'factory'           => 'Database\Factories',
+        'factory'           => 'Database\Factories\PreCadastro',
 
-        'repository_test'   => 'Tests\Repositories',
+        'repository_test'   => 'Tests\Repositories\PreCadastro',
 
-        'api_test'          => 'Tests\APIs',
+        'api_test'          => 'Tests\APIs\PreCadastro',
 
-        'tests'             => 'Tests',
+        'tests'             => 'Tests\PreCadastro',
     ],
 
     /*
@@ -132,11 +132,11 @@ return [
 
         'save_schema_file' => true,
 
-        'localized' => false,
+        'localized' => true,
 
-        'tables_searchable_default' => false,
+        'tables_searchable_default' => true,
 
-        'repository_pattern' => true,
+        'repository_pattern' => false,
 
         'resources' => false,
 
@@ -152,11 +152,11 @@ return [
 
     'prefixes' => [
 
-        'route' => '',  // using admin will create route('admin.?.index') type routes
+        'route' => 'precadastro',  // using admin will create route('admin.?.index') type routes
 
         'path' => '',
 
-        'view' => '',  // using backend will create return view('backend.?.index') type the backend views directory
+        'view' => 'precadastro',  // using backend will create return view('backend.?.index') type the backend views directory
 
         'public' => '',
     ],
@@ -172,9 +172,9 @@ return [
 
         'swagger'       => false,
 
-        'tests'         => true,
+        'tests'         => false,
 
-        'datatables'    => false,
+        'datatables'    => true,
 
         'menu'          => [
 
